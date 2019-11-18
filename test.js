94f4f44c9c45b99eacc5a642af86241ba543932b
uribo <ur1b0.dev@gmail.com>
2019-11-18 17:47:41 +0900

    onclickの練習

---
+++ test.js
@@ -0,0 +1,11 @@
+/*
+var click = function () {
+    let val = document.getElementById('section-2');
+    val.style.color = "red";
+}
+var myfunc = function () {
+    console.log("Hello world!");
+    let val = document.getElementById('section-2');
+    console.log(val);
+}
+*/
